2d7f91673fdbb8ccb0afc7066784ebb41dda8e8b
Jakob Westhoff <jakob@westhoffswelt.de>
2024-05-29 12:15:09 +0200

    Switch to stb-image for image reading

--- src/image_handling.rs
+++ src/image_handling.rs
@@ -1,5 +1,7 @@
+use anyhow::{anyhow, Context, Result};
 use image::ExtendedColorType;
-use anyhow::{Context, Result};
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 use crate::color::Color;
 
@@ -9,25 +11,46 @@ pub struct Image {
     pub height: usize,
 }
 
-pub fn load_image_from_file(input: &str) -> Result<Image> {
-    let image = image::io::Reader::open(input)
-        .context("open image {input} for reading")?
-        .decode()
-        .context("decode input image {input}")?
-        .into_rgb8();
-    let (width, height) = image.dimensions();
-    let buffer = image.into_raw();
-    let mut data = vec![Color::from(0xff); width as usize * height as usize];
-    for i in 0..buffer.len() / 3 {
-        data[i].r = buffer[i * 3];
-        data[i].g = buffer[i * 3 + 1];
-        data[i].b = buffer[i * 3 + 2];
+pub fn load_image_from_memory(bytes: &[u8]) -> Result<Image> {
+    use stb_image::image;
+    use stb_image::image::LoadResult::*;
+    match image::load_from_memory(bytes) {
+        Error(msg) => return Err(anyhow!("Could not load image from memory: {msg}")),
+        ImageF32(_) => return Err(anyhow!("Could not load hdr image from memory")),
+        ImageU8(image) => {
+            if image.depth != 3 {
+                return Err(anyhow!(
+                    "Could not load image with depth != 3. It has {depth}",
+                    depth = image.depth
+                ));
+            }
+
+            let mut buffer: Vec<Color> = Vec::with_capacity(image.width * image.height);
+            for i in (0..image.width * image.height * image.depth).step_by(image.depth) {
+                buffer.push(Color::from_rgb(
+                    image.data[i],
+                    image.data[i + 1],
+                    image.data[i + 2],
+                ))
+            }
+
+            return Ok(Image {
+                width: image.width,
+                height: image.height,
+                data: buffer,
+            });
+        }
     }
-    Ok(Image {
-        width: width as usize,
-        height: height as usize,
-        data,
-    })
+}
+
+pub fn load_image_from_file(input: &str) -> Result<Image> {
+    let mut reader =
+        BufReader::new(File::open(input).with_context(|| format!("open {input} for reading"))?);
+    let mut buffer: Vec<u8> = Vec::new();
+    reader
+        .read_to_end(&mut buffer)
+        .with_context(|| format!("read image data from {input}"))?;
+    load_image_from_memory(buffer.as_slice())
 }
 
 pub fn save_image_to_file(image: &Image, output: &str) -> Result<()> {
